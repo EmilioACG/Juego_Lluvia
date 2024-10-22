c20bb4af128ad592c8aef98614a1152cdcde90d0
MelissaRojas010 <melissa.rojas.g@mail.pucv.cl>
2024-10-22 19:37:45 -0300

    intento de rellenar comida

--- GameLluvia2024/core/src/main/java/puppy/code/Comida.java
+++ GameLluvia2024/core/src/main/java/puppy/code/Comida.java
@@ -1,4 +1,14 @@
 package puppy.code;
 
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+
 public abstract class Comida {
+    private Array<Rectangle> rainDropsPos;
+    private Array<Integer> rainDropsType;
+    private long lastDropTime;
+    private Sound dropSound;
+    private Music rainMusic;
 }
